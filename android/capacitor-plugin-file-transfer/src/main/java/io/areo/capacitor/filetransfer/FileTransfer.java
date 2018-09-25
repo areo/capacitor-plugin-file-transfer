630af6fcef255f5a0e02f24e47719a4e1bce96e5
Valentin Hăloiu <valentin.haloiu@areo.io>
2018-09-25 12:51:10 +0200

    Add basic Android implementation

--- android/capacitor-plugin-file-transfer/src/main/java/io/areo/capacitor/filetransfer/FileTransfer.java
+++ android/capacitor-plugin-file-transfer/src/main/java/io/areo/capacitor/filetransfer/FileTransfer.java
@@ -1,20 +1,58 @@
 package io.areo.capacitor.filetransfer;
 
-import com.getcapacitor.JSObject;
+import android.net.Uri;
+
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okio.BufferedSink;
+import okio.Okio;
+
 @NativePlugin()
 public class FileTransfer extends Plugin {
-
     @PluginMethod()
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void download(PluginCall call) {
+        String srcUrl = call.getString("url");
+        String dstFile = call.getString("file");
 
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+        try {
+            this.downloadToFile(srcUrl, dstFile);
+            call.success();
+        } catch (IOException e) {
+            call.error("FileTransfer download failed", e);
+        }
+    }
+
+    private void downloadToFile(String url, String filePath) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("HTTP response was not successful. Status code: " + response.code());
+        }
+
+        writeResponseToFile(response, filePath);
+    }
+
+    private void writeResponseToFile(Response response, String filePath) throws IOException {
+        File downloadedFile = new File(Uri.parse(filePath).getPath());
+        File parent = downloadedFile.getParentFile();
+        if (parent != null) {
+            parent.mkdirs();
+        }
+        BufferedSink sink = Okio.buffer(Okio.sink(downloadedFile));
+        sink.writeAll(response.body().source());
+        sink.close();
     }
 }
